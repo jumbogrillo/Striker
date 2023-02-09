1c235951cc622d94623f47e8ff7da57c0d5a96d3
GrandeCapo22 <100679162+GrandeCapo22@users.noreply.github.com>
2023-02-09 18:53:56 +0100

    Aggiunta classe Draw_Frame

---
+++ Striker_finale/Graphic.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stricker
+{
+	internal class Graphic
+	{
+		const int Margin_Top = 3;
+		const int Margin_Left = Margin_Top * 2;
+
+		public static void Rect(int x, int y, int width = 1, int height = 1, ConsoleColor bg = ConsoleColor.White)
+		{
+			Console.BackgroundColor = bg;
+			for (int i = 0; i < width; i++)
+				for (int j = 0; j < height; j++)
+				{
+					Console.SetCursorPosition((x + i) * 2, y + j);// 2 dimensione dei blocchi
+					Console.Write("  ");
+				}
+			Console.ResetColor();
+		}
+
+		public static void Draw_People()
+		{
+
+		}
+
+		public static void Draw_Frame(int width = 40, int height = 25, int margin_top = Margin_Top, int margin_left = Margin_Left)
+		{
+			width *= 2;
+			Console.SetCursorPosition(margin_left, margin_top);
+			Console.Write("╔");
+			for (int i = 0; i < width - 2; i++)
+			{
+				Console.Write("═");
+			}
+			Console.SetCursorPosition(margin_left, margin_top + 1);
+			for (int i = 1; i < height - 1; i++)
+			{
+				Console.SetCursorPosition(margin_left, margin_top + i);
+				Console.Write("║");
+			}
+			Console.SetCursorPosition(margin_left, margin_top + height - 1);
+			Console.Write("╚");
+			for (int i = 0; i < width - 2; i++)
+			{
+				Console.Write("═");
+			}
+			Console.Write("╝");
+			Console.SetCursorPosition(margin_left + width - 1, margin_top);
+			Console.Write("╗");
+			Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop - 1);
+			for (int i = 1; i < height - 1; i++)
+			{
+				Console.SetCursorPosition(margin_left + width - 1, margin_top + i);
+				Console.Write("║");
+			}
+		}
+	}
+}
---
+++ Striker_finale/Striker.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Stricker
+{
+    internal class Striker
+    {
+        public static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Thread title = new Thread(Title);
+            title.Start();
+            Thread sottofondo = new Thread(Music.SoundTrack);
+            bool musica = false;
+            if (Console.ReadKey().Key != ConsoleKey.M)
+            {
+                sottofondo.Start();
+                musica = true;
+            }
+            title.Abort();
+            Console.ResetColor();
+
+            Console.Clear();
+            int indexmenu = 0;
+            bool next = false;
+            ConsoleKey input;
+            while (!next)
+            {
+                Console.SetCursorPosition(40, indexmenu);
+                Console.Clear();
+                Menu(indexmenu);
+                input = Console.ReadKey().Key;
+                if (input == ConsoleKey.UpArrow && indexmenu > 0)
+                {
+                    indexmenu--;
+                }
+                else if (input == ConsoleKey.DownArrow && indexmenu < 2)
+                {
+                    indexmenu++;
+                }
+                else if (input == ConsoleKey.Enter)
+                {
+                    next = true;
+                }
+            }
+        }
+
+
+
+
+
+
+
+        public static void Menu(int index)
+        {
+            Console.WriteLine(@"   
+     _______.___________.______      __   __  ___  _______ .______      
+    /       |           |   _  \    |  | |  |/  / |   ____||   _  \     
+   |   (----`---|  |----|  |_)  |   |  | |  '  /  |  |__   |  |_)  |    
+    \   \       |  |    |      /    |  | |    <   |   __|  |      /     
+.----)   |      |  |    |  |\  \----|  | |  .  \  |  |____ |  |\  \----.
+|_______/       |__|    | _| `._____|__| |__|\__\ |_______|| _| `._____|");
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                if (index == i)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                switch (i)
+                {
+                    case 0:
+                        Console.WriteLine("Gioca");
+                        break;
+                    case 1:
+                        Console.WriteLine("Seleziona Tema");
+                        break;
+                    case 2:
+                        Console.WriteLine("Esci");
+                        break;
+                }
+                Console.ResetColor();
+            }
+        }
+        public static void Title()
+        {
+            //TITLE LOGO
+            int title_color = 0;
+            while (true)
+            {
+                Console.SetCursorPosition(0, 0);
+                if (title_color == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                }
+
+                Console.WriteLine($@" 
+            ╔══════════════════════════════════════════════════════════════════════════════════════════╗
+            ║             _______.___________..______       __   __  ___  _______ .______              ║
+            ║            /       |           ||   _  \     |  | |  |/  / |   ____||   _  \             ║
+            ║           |   (----`---|  |----`|  |_)  |    |  | |  '  /  |  |__   |  |_)  |            ║ 
+            ║            \   \       |  |     |      /     |  | |    <   |   __|  |      /             ║
+            ║        .----)   |      |  |     |  |\  \----.|  | |  .  \  |  |____ |  |\  \----.        ║ 
+            ║        |_______/       |__|     | _| `._____||__| |__|\__\ |_______|| _| `._____|        ║  
+            ║                                                                                          ║
+            ║                                                                                          ║      
+            ║                                                                                          ║              
+            ║                                                                                          ║ 
+            ║                                                                                          ║
+            ╚══════════════════════════════════════════════════════════════════════════════════════════╝
+
+
+");
+                Console.ResetColor();
+                Console.SetCursorPosition(45, 9);
+                Console.Write("Premi un tasto per giocare");
+                Console.SetCursorPosition(42, 10);
+                Console.Write("Premi M per giocare senza sonoro");
+                title_color++;
+                Thread.Sleep(100);
+                if (title_color == 2)
+                {
+                    title_color = 0;
+                }
+            }
+        }
+    }
+}
