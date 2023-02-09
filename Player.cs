69a5f9d4a2ebe86e3fb2eb9b2f150189295b2002
jumbogrillo <81551432+jumbogrillo@users.noreply.github.com>
2023-02-09 18:37:13 +0100

    Aggiungere i file di progetto.

---
+++ Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Stricker
+{
+	class Player
+	{
+
+
+		private int[] position = new int[2];
+		private ConsoleColor color;
+		private int score;
+		private int combo;
+		private int speed;
+		private int life;
+
+		public int[] Position { get => position; set => position = value; }
+		public ConsoleColor Color { get => color; set => color = value; }
+		public int Score { get => score; set => score = value; }
+		public int Combo { get => combo; set => combo = value; }
+		public int Speed { get => speed; set => speed = value; }
+		public int Life { get => life; set => life = value; }
+
+
+	}
+
+
+}
